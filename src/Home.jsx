29e1911df8a72ff01bea7eed260892a091d30094
keji <tomitoyo.93@gmail.com>
2021-03-10 05:05:22 +0100

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,33 @@
-import { useState } from 'react'
-import BlogList from './BlogList'
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
-    return ( 
-        <div className="home">
-       <BlogList blogs={blogs} title ='All Blogs!' handleDelete = {handleDelete}/>
-       </div>
-     );
-}
- 
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+    {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
+
 export default Home;
