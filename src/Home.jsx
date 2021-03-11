306e6488beeb550f931c44a33d37a4d737628042
keji <tomitoyo.93@gmail.com>
2021-03-11 06:24:31 +0100

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,15 +5,24 @@ import Loading from './Loading'
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     setTimeout(() =>{
       fetch("http://localhost:8000/blogs")
     .then((res) => {
+      if(!res.ok){
+        throw Error('could not load data')
+      }
       return res.json();
     })
     .then((data) => {
       setBlogs(data);
+      setLoading(false)
+      setError(null)
+    })
+    .catch(err => {
+      setError(err.message)
       setLoading(false)
     })
     }, 7000);
@@ -21,6 +30,7 @@ const Home = () => {
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {loading && <Loading/>}
     {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
